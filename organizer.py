64b2f8f7b43cde764d383a9c4a10833d542b155a
lucarichardsen <luca.work@web.de>
2023-06-07 20:11:38 +0200

    Gui added

--- organizer.py
+++ organizer.py
@@ -1,26 +1,51 @@
 import os
 import shutil
-
-directory = input("Path of Organize directory: ")
+import tkinter as tk
+from tkinter import filedialog as fd
 
 class Organizer:
 
+    def gui():
+        root = tk.Tk()
+        root.title('Select Folder')
+        root.resizable(False, False)
+        root.geometry('350x150')
+
+
+
+        def select_folder():
+            folder = fd.askdirectory(
+                title='Select directory')
+            Organizer.organize(folder)
+
+        def quit():
+            exit()
+
+        open_btn = tk.Button(root, text='Select a Folder', command=select_folder)
+        quit_btn = tk.Button(root, text='Quit', command=quit)
+        open_btn.pack(expand=True)
+        quit_btn.pack(expand=True)
+        # run the application
+        root.mainloop()
+
+
     def organize(path):
-        try:
-            files = os.listdir(path)
-            for file in files:
-                filename, extentions = os.path.splitext(file)
-                extentions = extentions[1:]
-                move_from = path + "/" + file
-                move_to = path + "/" + extentions + "/" + file
-                c = path + "/" + extentions
-                if os.path.exists(c):
-                    shutil.move(move_from, move_to)
-                else:
-                    os.makedirs(c)
-                    shutil.move(move_from, move_to)
-        except PermissionError:
-            print("You dont have permission")
+
+            try:
+                files = os.listdir(path)
+                for file in files:
+                    filename, extentions = os.path.splitext(file)
+                    extentions = extentions[1:]
+                    move_from = path + "/" + file
+                    move_to = path + "/" + extentions + "/" + file
+                    c = path + "/" + extentions
+                    if os.path.exists(c):
+                        shutil.move(move_from, move_to)
+                    else:
+                        os.makedirs(c)
+                        shutil.move(move_from, move_to)
+            except PermissionError:
+                print("You dont have permission")
                     
 
-Organizer.organize(directory)
+Organizer.gui()
